54088ed7e6dde25f10402cfdc27577b2df43040e
raihannishat.swe@gmail.com <en1152nasir>
2020-06-02 16:17:45 +0600

    Class 2 Code

---
+++ Class_2/BasaicOOP/BasaicOOP/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BasaicOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new Student("182-35-2518", "Raihan Nishat", 3.56);
+            //student.selectCredit();
+            //student.takeCourse();
+            student.Display();
+        }
+    }
+}
