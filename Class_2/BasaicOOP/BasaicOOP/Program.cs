be9c247a037099ec65dd40e8275f41376e3a237e
raihannishat.swe@gmail.com <en1152nasir>
2020-06-02 16:32:12 +0600

    Object and Class

--- Class_2/BasaicOOP/BasaicOOP/Program.cs
+++ Class_2/BasaicOOP/BasaicOOP/Program.cs
@@ -6,7 +6,7 @@ namespace BasaicOOP
     {
         static void Main(string[] args)
         {
-            var student = new Student("182-35-2518", "Raihan Nishat", 3.56);
+            var student = new Student("182-35-2518", "Raihan Nishat", 3.65);
             //student.selectCredit();
             //student.takeCourse();
             student.Display();
