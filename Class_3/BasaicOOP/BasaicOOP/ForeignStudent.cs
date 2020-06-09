6e06dcedef15db472eb815d9a0e9d5a5025df71d
raihannishat.swe@gmail.com <en1152nasir>
2020-06-09 14:36:44 +0600

    Class_3

---
+++ Class_3/BasaicOOP/BasaicOOP/ForeignStudent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasaicOOP
+{
+    public class ForeignStudent : Student
+    {
+        public ForeignStudent(string id, string name, double cgpa) : base(id, name, cgpa)
+        {
+
+        }
+    }
+}
