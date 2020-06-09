287783c22ad080641c909c3fed92ea80486d0b8b
raihannishat.swe@gmail.com <en1152nasir>
2020-06-09 14:35:03 +0600

    Class_3

---
+++ Class_3/BasaicOOP/Faculty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasaicOOP
+{
+    public class Faculty
+    {
+        public string id { get; set; }
+        public string department { get; set; }
+        public double salary { get; set; }
+
+        public void regesterStudents()
+        {
+            Console.WriteLine("Regester of all students");
+        }
+    }
+}
---
+++ Class_3/BasaicOOP/LocalStudent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasaicOOP
+{
+    public class LocalStudent : Student
+    {
+        public LocalStudent(string id, string name, double cgpa) : base(id, name, cgpa)
+        {
+
+        }
+    }
+}
