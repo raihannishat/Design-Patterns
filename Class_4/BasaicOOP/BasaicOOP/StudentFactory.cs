c1a0bade9ee36053158e98fce626d8f371917181
raihannishat.swe@gmail.com <en1152nasir>
2020-06-16 14:40:33 +0600

    Class_4

---
+++ Class_4/BasaicOOP/BasaicOOP/StudentFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasaicOOP
+{
+    public class StudentFactory
+    {
+        public Student GetStudent(string option)
+        {
+            Student student = null;
+
+            if (option.Equals("Local Student"))
+            {
+                student = new LocalStudent();
+            }
+            else if (option.Equals("Foreign Student"))
+            {
+                student = new ForeignStudent();
+            }
+            else
+            {
+                return null;
+            }
+
+            return student;
+        }
+    }
+}
