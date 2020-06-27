512ed39007ecb58ea18200c8b3a33ad212b2ba65
raihannishat.swe@gmail.com <en1152nasir>
2020-06-27 17:04:31 +0600

    Question_1

---
+++ Quiz_1/Question_1/Quiz_1/Corner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quiz_1
+{
+    public abstract class Corner
+    {
+        public string shopnumber { get; set; }
+        public string discountStatus { get; set; }
+
+        public abstract void DisplayProduct();
+        public abstract void BuyProduct(Product product);
+    }
+}
