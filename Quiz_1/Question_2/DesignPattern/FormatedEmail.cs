895be8f085baa04adf14bb534452fca4993977bb
raihannishat.swe@gmail.com <en1152nasir>
2020-06-27 17:09:45 +0600

    Question_2

---
+++ Quiz_1/Question_2/DesignPattern/FormatedEmail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern
+{
+    public class FormatedEmail : IEmailService
+    {
+        public void SendEmail(string address, string subject, string body)
+        {
+            Console.WriteLine($"{address} {subject} {body}");
+        }
+    }
+}
