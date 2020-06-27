d0f2c2716a6a971d4c173927ba839de70c1a681c
raihannishat.swe@gmail.com <en1152nasir>
2020-06-27 17:04:58 +0600

    Question_2

---
+++ Quiz_1/Question_2/DesignPattern/DesignPattern/EmailService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern
+{
+    public class EmailService : IEmailService
+    {
+        public void SendEmail(string address, string subject, string body)
+        {
+            Console.WriteLine("{0}", address);
+            Console.WriteLine("{0}", subject);
+            Console.WriteLine("{0}", body);
+        }
+    }
+}
