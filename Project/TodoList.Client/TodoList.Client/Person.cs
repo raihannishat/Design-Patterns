06bbc4675bfb8225c85e956d4b4d7dee831d755e
raihannishat.swe@gmail.com <en1152nasir>
2020-07-03 00:39:40 +0600

    This is test object for SqlBuilder Test

---
+++ Project/TodoList.Client/TodoList.Client/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoList.Client
+{
+    public class Person
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public string cgpa { get; set; }
+    }
+}
---
+++ Project/TodoList.Client/TodoList.Client/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoList.Client
+{
+    public class Product
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+        public int quantity { get; set; }
+    }
+}
