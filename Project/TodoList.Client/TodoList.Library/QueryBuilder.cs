17531b4d9443fabe584643c6a42ad2c8aaa8f6b1
raihannishat.swe@gmail.com <en1152nasir>
2020-07-03 00:38:47 +0600

    Builder Pattern use this class

---
+++ Project/TodoList.Client/TodoList.Library/QueryBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoList.Library
+{
+    public class QueryBuilder
+    {
+        private string _query;
+
+        public void Add(string value)
+        {
+            _query = _query + value;
+        }
+
+        public string GetQuery()
+        {
+            return _query;
+        }
+    }
+}
