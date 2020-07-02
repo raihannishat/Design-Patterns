647116caa693d536a26be6eb085936de3dba681a
raihannishat.swe@gmail.com <en1152nasir>
2020-07-02 22:59:11 +0600

    Class_7

---
+++ Class_7/SingletonDesignPattern/SingletonDesignPattern/SingletonPattern.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonDesignPattern
+{
+    public class SingletonPattern
+    {
+        private volatile static SingletonPattern instance;
+        private static readonly object _look = new object();
+
+        private SingletonPattern() { }
+
+        public static SingletonPattern GetSingletonPattern()
+        {
+            if (instance == null)
+            {
+                lock(_look)
+                {
+                    if (instance == null)
+                    {
+                        instance = new SingletonPattern();
+                    }
+                }
+            }
+
+            return instance;
+        }
+    }
+}
